e3db10a85e5e49a2c629442d677ef7f992928d12
roikoren755 <roi@altnext.com>
2021-12-02 12:03:54 +0200

    test: fix

--- docs/rules/no-top-level-await.md
+++ docs/rules/no-top-level-await.md
@@ -16,5 +16,5 @@ await expr;
 
 ## 📚 References
 
-- [Rule source](https://github.com/roikoren755/eslint-plugin-es/blob/v0.0.4/src/rules/no-top-level-await.ts)
-- [Test source](https://github.com/roikoren755/eslint-plugin-es/blob/v0.0.4/tests/src/rules/no-top-level-await.ts)
+- [Rule source](https://github.com/roikoren755/eslint-plugin-es/blob/v0.0.6/src/rules/no-top-level-await.ts)
+- [Test source](https://github.com/roikoren755/eslint-plugin-es/blob/v0.0.6/tests/src/rules/no-top-level-await.ts)
--- tests/src/rules/no-top-level-await.ts
+++ tests/src/rules/no-top-level-await.ts
@@ -1,48 +1,52 @@
+import type { TSESLint } from '@typescript-eslint/experimental-utils';
 import { AST_NODE_TYPES } from '@typescript-eslint/types';
 
 import { RuleTester } from '../../tester';
 import rule from '../../../src/rules/no-top-level-await';
 
-const error = { messageId: 'forbidden' as const, line: 1, column: 1, type: AST_NODE_TYPES.MemberExpression, data: {} };
+const error = { messageId: 'forbidden' as const, line: 1, column: 1, type: AST_NODE_TYPES.AwaitExpression, data: {} };
 
 if (!RuleTester.isSupported(2022)) {
   console.log('Skip the tests of no-top-level-await.');
 } else {
-  new RuleTester().run('no-top-level-await', rule, {
-    valid: [
-      'async function f() { await expr }',
-      'expr;',
-      'const f = async function() { await expr }',
-      'const f = async () => { await expr }',
-      '({ async method() { await expr } })',
-      'class A { async method() { await expr } }',
-      '(class { async method() { await expr } })',
-      'async function f() { for await (a of b); }',
-      'async function f() { for await (var a of b); }',
-      'async function f() { for await (let a of b); }',
-      'async function f() { for await (const a of b); }',
-      'function f() { async function f() { await expr } }',
-    ],
-    invalid: [
-      { code: 'await expr', errors: [{ ...error, column: 1 }] },
-      { code: 'for await (a of b);', errors: [{ ...error, column: 1 }] },
-      { code: 'for await (var a of b);', errors: [{ ...error, column: 1 }] },
-      { code: 'for await (let a of b);', errors: [{ ...error, column: 1 }] },
-      { code: 'for await (const a of b);', errors: [{ ...error, column: 1 }] },
-      {
-        code: `
+  new RuleTester({ parserOptions: { sourceType: 'module' } } as TSESLint.RuleTesterConfig).run(
+    'no-top-level-await',
+    rule,
+    {
+      valid: [
+        'async function f() { await expr }',
+        'expr;',
+        'const f = async function() { await expr }',
+        'const f = async () => { await expr }',
+        '({ async method() { await expr } })',
+        'class A { async method() { await expr } }',
+        '(class { async method() { await expr } })',
+        'async function f() { for await (a of b); }',
+        'async function f() { for await (var a of b); }',
+        'async function f() { for await (let a of b); }',
+        'async function f() { for await (const a of b); }',
+        'function f() { async function f() { await expr } }',
+      ],
+      invalid: [
+        { code: 'await expr()', errors: [{ ...error }] },
+        { code: 'for await (a of b);', errors: [{ ...error, type: AST_NODE_TYPES.ForOfStatement }] },
+        { code: 'for await (var a of b);', errors: [{ ...error, type: AST_NODE_TYPES.ForOfStatement }] },
+        { code: 'for await (let a of b);', errors: [{ ...error, type: AST_NODE_TYPES.ForOfStatement }] },
+        { code: 'for await (const a of b);', errors: [{ ...error, type: AST_NODE_TYPES.ForOfStatement }] },
+        {
+          code: `
 await expr
 async function f() {
   await expr
 }
 await expr`,
-        errors: [
-          { ...error, line: 2, column: 1 },
-          { ...error, line: 6, column: 1 },
-        ],
-      },
-      {
-        code: `
+          errors: [
+            { ...error, line: 2 },
+            { ...error, line: 6 },
+          ],
+        },
+        {
+          code: `
 await expr
 async function f() {
   await expr
@@ -51,29 +55,30 @@ async function f() {
   }
 }
 await expr`,
-        errors: [
-          { ...error, line: 2, column: 1 },
-          { ...error, line: 9, column: 1 },
-        ],
-      },
-      {
-        code: `
+          errors: [
+            { ...error, line: 2 },
+            { ...error, line: 9 },
+          ],
+        },
+        {
+          code: `
 let jQuery;
 try {
   jQuery = await import('https://cdn-a.com/jQuery');
 } catch {
   jQuery = await import('https://cdn-b.com/jQuery');
 }`,
-        errors: [
-          { ...error, line: 4, column: 1 },
-          { ...error, line: 6, column: 1 },
-        ],
-      },
-      { code: '{ await expr }', errors: [{ ...error, column: 1 }] },
-      { code: '( await expr )', errors: [{ ...error, column: 1 }] },
-      { code: 'fn( await expr )', errors: [{ ...error, column: 1 }] },
-      { code: 'if (foo) { await expr }', errors: [{ ...error, column: 1 }] },
-      { code: 'for (const foo of bar) { await expr }', errors: [{ ...error, column: 1 }] },
-    ],
-  });
+          errors: [
+            { ...error, line: 4, column: 12 },
+            { ...error, line: 6, column: 12 },
+          ],
+        },
+        { code: '{ await expr }', errors: [{ ...error, column: 3 }] },
+        { code: '( await expr )', errors: [{ ...error, column: 3 }] },
+        { code: 'fn( await expr )', errors: [{ ...error, column: 5 }] },
+        { code: 'if (foo) { await expr }', errors: [{ ...error, column: 12 }] },
+        { code: 'for (const foo of bar) { await expr }', errors: [{ ...error, column: 26 }] },
+      ],
+    },
+  );
 }
